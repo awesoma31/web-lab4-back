26142bdcbea43aa221ae4cb938de5a57088ece01
awesoma31 <gwerty31102005@gmail.com>
2024-11-03 14:35:58 +0300

    init commit

---
+++ src/main/java/org/awesoma/lab4back/Lab4BackApplication.java
@@ -0,0 +1,13 @@
+package org.awesoma.lab4back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/awesoma/lab4back/Lab4BackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.awesoma.lab4back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
